3ea39dbfb9dfbe7de482b19bf682236b9c46385e
rengakrishnaa <114340951+rengakrishnaa@users.noreply.github.com>
2023-01-23 17:19:01 +0530

    Updated..

---
+++ Scanning.py
@@ -0,0 +1,14 @@
+import socket
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = "192.168.164.1"
+port = 443
+
+def portscanner(port):
+        if sock.connect_ex((host,port)):
+                print ("Port %d is closed") % (port)
+        else:
+                print("Port %d is open") % (port)
+
+portscanner(port)
